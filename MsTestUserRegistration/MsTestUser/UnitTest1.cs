f5b60ea5448a8881a066d55408e8231a6c09ed9c
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-14 20:23:10 +0530

    Added: To check Lastname

--- MsTestUserRegistration/MsTestUser/UnitTest1.cs
+++ MsTestUserRegistration/MsTestUser/UnitTest1.cs
@@ -7,12 +7,21 @@ namespace MsTestUser
     public class UnitTest1
     {
         string firstName = "^[A-Z]{1}[a-z]{2,10}$";
+        string lastName = "^[A-Z]{1}[a-z]{2,10}$";
         UserDetails us = new UserDetails();
         [TestMethod]
         public void TestMethod1()
         {
             var result = us.FirstName("Shruti", firstName);
             Assert.AreEqual(true, result.Success);
+        }
+        //UC2 for last name
+        [TestMethod]
+        public void Test2()
+        {
+            var result = us.LastName("Belsare", lastName);
+            Assert.AreEqual(true, result.Success);
+
         }
     }
 }
